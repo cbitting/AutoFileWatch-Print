0bc49c1080b1a28fcce37b09f35e0129e838ab94
Chris Bitting <chris.bitting@gmail.com>
2016-05-11 17:02:49 -0400

    Initial Version

---
+++ autoFilePrinter/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace autoFilePrinter
+{
+    partial class frmFilePrinter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDirToWatch = new System.Windows.Forms.TextBox();
+            this.lblDir = new System.Windows.Forms.Label();
+            this.lblExt = new System.Windows.Forms.Label();
+            this.txtWatchExt = new System.Windows.Forms.TextBox();
+            this.lstHistory = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chkPrintOnce = new System.Windows.Forms.CheckBox();
+            this.lblPrintOnce = new System.Windows.Forms.Label();
+            this.btnWatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDirToWatch
+            // 
+            this.txtDirToWatch.Location = new System.Drawing.Point(11, 28);
+            this.txtDirToWatch.Name = "txtDirToWatch";
+            this.txtDirToWatch.Size = new System.Drawing.Size(426, 20);
+            this.txtDirToWatch.TabIndex = 0;
+            this.txtDirToWatch.Text = "E:\\cbitting\\testprint";
+            // 
+            // lblDir
+            // 
+            this.lblDir.AutoSize = true;
+            this.lblDir.Location = new System.Drawing.Point(12, 9);
+            this.lblDir.Name = "lblDir";
+            this.lblDir.Size = new System.Drawing.Size(151, 13);
+            this.lblDir.TabIndex = 1;
+            this.lblDir.Text = "Folder To Watch (ie: C:\\folder)";
+            // 
+            // lblExt
+            // 
+            this.lblExt.AutoSize = true;
+            this.lblExt.Location = new System.Drawing.Point(12, 61);
+            this.lblExt.Name = "lblExt";
+            this.lblExt.Size = new System.Drawing.Size(152, 13);
+            this.lblExt.TabIndex = 3;
+            this.lblExt.Text = "Extension To Look For (ie: pdf)";
+            // 
+            // txtWatchExt
+            // 
+            this.txtWatchExt.Location = new System.Drawing.Point(11, 80);
+            this.txtWatchExt.Name = "txtWatchExt";
+            this.txtWatchExt.Size = new System.Drawing.Size(230, 20);
+            this.txtWatchExt.TabIndex = 2;
+            this.txtWatchExt.Text = "txt";
+            // 
+            // lstHistory
+            // 
+            this.lstHistory.FormattingEnabled = true;
+            this.lstHistory.Location = new System.Drawing.Point(15, 141);
+            this.lstHistory.Name = "lstHistory";
+            this.lstHistory.Size = new System.Drawing.Size(422, 173);
+            this.lstHistory.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "History";
+            // 
+            // chkPrintOnce
+            // 
+            this.chkPrintOnce.AutoSize = true;
+            this.chkPrintOnce.Location = new System.Drawing.Point(265, 80);
+            this.chkPrintOnce.Name = "chkPrintOnce";
+            this.chkPrintOnce.Size = new System.Drawing.Size(141, 17);
+            this.chkPrintOnce.TabIndex = 6;
+            this.chkPrintOnce.Text = "Print Filename One Time";
+            this.chkPrintOnce.UseVisualStyleBackColor = true;
+            // 
+            // lblPrintOnce
+            // 
+            this.lblPrintOnce.AutoSize = true;
+            this.lblPrintOnce.Location = new System.Drawing.Point(250, 64);
+            this.lblPrintOnce.Name = "lblPrintOnce";
+            this.lblPrintOnce.Size = new System.Drawing.Size(187, 13);
+            this.lblPrintOnce.TabIndex = 7;
+            this.lblPrintOnce.Text = "Print Files Once (uses filename history)";
+            // 
+            // btnWatch
+            // 
+            this.btnWatch.Location = new System.Drawing.Point(314, 324);
+            this.btnWatch.Name = "btnWatch";
+            this.btnWatch.Size = new System.Drawing.Size(123, 33);
+            this.btnWatch.TabIndex = 8;
+            this.btnWatch.Text = "Watch";
+            this.btnWatch.UseVisualStyleBackColor = true;
+            this.btnWatch.Click += new System.EventHandler(this.btnWatch_Click);
+            // 
+            // frmFilePrinter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 369);
+            this.Controls.Add(this.btnWatch);
+            this.Controls.Add(this.lblPrintOnce);
+            this.Controls.Add(this.chkPrintOnce);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstHistory);
+            this.Controls.Add(this.lblExt);
+            this.Controls.Add(this.txtWatchExt);
+            this.Controls.Add(this.lblDir);
+            this.Controls.Add(this.txtDirToWatch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmFilePrinter";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Auto File Print";
+            this.Load += new System.EventHandler(this.frmFilePrinter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDirToWatch;
+        private System.Windows.Forms.Label lblDir;
+        private System.Windows.Forms.Label lblExt;
+        private System.Windows.Forms.TextBox txtWatchExt;
+        private System.Windows.Forms.ListBox lstHistory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox chkPrintOnce;
+        private System.Windows.Forms.Label lblPrintOnce;
+        private System.Windows.Forms.Button btnWatch;
+    }
+}
+
---
+++ autoFilePrinter/Form1.cs
@@ -0,0 +1,166 @@
+﻿using autoFilePrinter.Properties;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace autoFilePrinter
+{
+    public partial class frmFilePrinter : Form
+    {
+        private delegate void UpdateHistoryDel(string newText);
+
+        private FileSystemWatcher fsWatcher;
+        private static object fileLock = new object();
+
+        public frmFilePrinter()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFilePrinter_Load(object sender, EventArgs e)
+        {
+            if (Settings.Default.Properties.Cast<SettingsProperty>().Any(prop => prop.Name == "WatchDir"))
+            {
+                txtDirToWatch.Text = (string)Settings.Default["WatchDir"];
+            }
+
+            if (Settings.Default.Properties.Cast<SettingsProperty>().Any(prop => prop.Name == "WatchExt"))
+            {
+                txtWatchExt.Text = (string)Settings.Default["WatchExt"];
+            }
+            if (Settings.Default.Properties.Cast<SettingsProperty>().Any(prop => prop.Name == "PrintOnce"))
+            {
+                chkPrintOnce.Checked = (bool)Settings.Default["PrintOnce"];
+            }
+            addHistory("Application Started");
+        }
+
+        private void btnWatch_Click(object sender, EventArgs e)
+        {
+            if (fsWatcher == null)
+            {
+                startWatching();
+                btnWatch.Text = "Stop Watching";
+            }
+            else
+            {
+                fsWatcher.EnableRaisingEvents = false;
+                fsWatcher = null;
+                btnWatch.Text = "Watch";
+            }
+        }
+
+        private void startWatching()
+        {
+            if (Directory.Exists(txtDirToWatch.Text))
+            {
+                Settings.Default["WatchDir"] = txtDirToWatch.Text;
+                Settings.Default["WatchExt"] = txtWatchExt.Text;
+                Settings.Default["PrintOnce"] = chkPrintOnce.Checked;
+
+                Properties.Settings.Default.Save();
+
+                fsWatcher = new FileSystemWatcher(txtDirToWatch.Text);
+
+                fsWatcher.Created += new System.IO.FileSystemEventHandler(this.OnChanged);
+
+                var withFSW = fsWatcher;
+                withFSW.EnableRaisingEvents = true;
+                withFSW.IncludeSubdirectories = false;
+
+                withFSW.Filter = "*." + txtWatchExt.Text;
+
+                this.BeginInvoke(new UpdateHistoryDel(addHistory), "Watching: " + txtDirToWatch.Text + " for: " + withFSW.Filter);
+            }
+            else
+            {
+                this.BeginInvoke(new UpdateHistoryDel(addHistory), "Directory Doesn't Exist!: " + txtDirToWatch.Text);
+            }
+        }
+
+        public void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            //print
+            printFile(e.FullPath);
+        }
+
+        public void printFile(string fpath)
+        {
+            //should it check for dupes?
+            bool isDupe = false;
+
+            if (chkPrintOnce.Checked)
+            {
+                foreach (string file in File.ReadAllLines(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\autoprintlog.txt"))
+                {
+                    if (fpath.ToLower() == file.ToLower())
+                    {
+                        isDupe = true;
+                    }
+                }
+            }
+
+            if (!isDupe)
+            {
+                //print the file:
+                Process PrintProcess = new Process();
+                PrintProcess.StartInfo.CreateNoWindow = false;
+                PrintProcess.StartInfo.Verb = "print";
+                PrintProcess.StartInfo.FileName = fpath;
+                PrintProcess.Start();
+
+                UpdateFile fileupdate = new UpdateFile { filePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\autoprintlog.txt", lineData = fpath };
+                //add the queue item
+                ThreadPool.QueueUserWorkItem(UpdateFile, fileupdate);
+                this.BeginInvoke(new UpdateHistoryDel(addHistory), "Printed: " + fpath);
+            }
+            else
+            {
+                this.BeginInvoke(new UpdateHistoryDel(addHistory), "Duplicate: " + fpath);
+            }
+        }
+
+        public bool isDupe(string fname)
+        {
+            bool dupe = false;
+            string lfile = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            return dupe;
+        }
+
+        public void saveLog(string fname)
+        {
+        }
+
+        private static void UpdateFile(object fileData)
+        {
+            //set object to be locked
+            lock (fileLock)
+            {
+                //get the file
+                using (var fw = File.AppendText(((UpdateFile)fileData).filePath))
+                {
+                    //write the data
+                    fw.WriteLine(((UpdateFile)fileData).lineData);
+                }
+            }
+        }
+
+        public void addHistory(string htxt)
+        {
+            lstHistory.Items.Insert(0, DateTime.Now.ToString() + " - " + htxt);
+        }
+    }
+
+    public class UpdateFile
+    {
+        public string lineData { get; set; }
+
+        public string filePath { get; set; }
+    }
+}
